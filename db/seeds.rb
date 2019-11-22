3609b2d0269240e145b846015c2b12a75b1925de
github username <github email address>
2019-11-22 17:19:47 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Destroying"
+Student.destroy_all
+puts "Starting Creation"
+s1 = Student.create(first_name:"Bob",last_name:"Marley")
+s2 = Student.create(first_name:"Rob",last_name:"Marley")
+s3 = Student.create(first_name:"Cob",last_name:"Marley")
+s4 = Student.create(first_name:"Sob",last_name:"Marley")
+s5 = Student.create(first_name:"Mob",last_name:"Marley")
+s6 = Student.create(first_name:"Pob",last_name:"Marley")
+s7 = Student.create(first_name:"Fob",last_name:"Marley")
+puts "Finished Creation"
+
