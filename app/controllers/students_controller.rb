03e43b94f03d63a26dec991032b52ee4508bb4d4
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-11-25 14:51:02 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,17 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def activate
+    puts "Params: #{params}"
+    student = Student.find(params[:id])
+    student.active = !student.active
+    puts "Student: #{student.active}" #forgot to save
+
+    student.save
+
+    redirect_to student_path(student)
+  end
+
   def show
   end
 
